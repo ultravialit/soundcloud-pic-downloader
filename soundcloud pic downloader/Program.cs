aac9e50677db533e414bf8f81329e5d377ddf43d
despa1r1337 <despairov@yandex.ru>
2021-05-24 18:44:01 +0300

    file added

---
+++ soundcloud pic downloader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace soundcloud_pic_downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("enter soundcloud link to the music: ");
+                string adress = Console.ReadLine();
+                string filename;
+                WebClient web = new WebClient();
+                string scr = web.DownloadString(adress);
+
+                var match = Regex.Match(scr, "\"twitter\\:image\"\\s+content=\"([^\"]+)"); // thanks  earskilla
+                if (!match.Success)
+                {
+                    Console.WriteLine("!match.Success");
+                }
+                else
+                {
+                    string output = match.Groups[1].Value;
+                    string datatime = DateTime.Now.ToString("yyyy-MM-dd");
+                    filename = datatime + ".jpg";
+                    web.DownloadFile(output, filename);
+                    Console.Clear();
+                    Console.WriteLine("pic saved " + output + " with name " + filename);
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
+
